2a478d86b571d3543be5b4be96ca69b28e6f7ab4
Sang Le <odiegarfield11@gmail.com>
2022-04-15 15:44:31 +0700

    Coding for GetWeatherForecast

--- Web Api/Controllers/WeatherForecastController.cs
+++ Web Api/Controllers/WeatherForecastController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Web_Api.Controllers
 {
@@ -8,7 +11,7 @@ namespace Web_Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracin g", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -19,15 +22,35 @@ namespace Web_Api.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<WeatherForecast> GetAsync()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            using (var client = new HttpClient())
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                var key = "43becd2fb33d465886e80749221504";
+                var uri = "https://api.weatherapi.com/v1/forecast.xml";
+                var q = "London";
+                var days = "1";
+                var aqi = "no";
+                var alerts = "no";
+                client.BaseAddress = new Uri(uri);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+                HttpResponseMessage response = await client.GetAsync($"?key={key}&q={q}&days={days}&aqi={aqi}&alerts={alerts}");
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    var xmlResponse = XElement.Parse(responseBody);
+                    var temperature = xmlResponse.Descendants().First(node => node.Name == "avgtemp_c").Value;
+                    var condition = xmlResponse.Descendants().FirstOrDefault(xmlResponse => xmlResponse.Name == "condition");
+                    var weather = condition.Descendants().First(node => node.Name == "text").Value;
+
+                    return new WeatherForecast
+                    {
+                        Temperature = temperature,
+                        Weather = temperature
+                    };
+                }
+                return null;
+            }
         }
     }
 }
--- Web Api/WeatherForecast.cs
+++ Web Api/WeatherForecast.cs
@@ -2,12 +2,8 @@ namespace Web_Api
 {
     public class WeatherForecast
     {
-        public DateTime Date { get; set; }
+        public string Temperature{ get; set; }
+        public string Weather { get; set; }
 
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string? Summary { get; set; }
     }
 }
